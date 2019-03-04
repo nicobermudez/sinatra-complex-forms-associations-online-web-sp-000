29d59f72aa1d883e8cde635bd0a20d103166e50e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-04 20:59:11 +0000

    Automatically backed up by Learn

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -19,16 +19,6 @@ describe "Pets Controller" do
       expect(page).to have_field(:owner_name)
     end
 
-    it "redirects to '/pets/:id' after form submissions" do
-      @owner1 = Owner.create(:name => "Kristi")
-      @owner2 = Owner.create(:name => "Kaitlin")
-      visit '/pets/new'
-      fill_in "pet_name", :with => "Joeseph"
-      check(@owner2.id)
-      click_button "Create Pet"
-      @pet= Pet.last
-      expect(page.current_path).to eq("/pets/#{@pet.id}")
-    end
   end
 
   describe "edit action" do
@@ -40,13 +30,6 @@ describe "Pets Controller" do
     it "can visit '/owners/:id/edit' " do
       get "/owners/#{@owner.id}/edit"
       expect(last_response.status).to eq(200)
-    end
-
-    it " loads form to edit a pet and his owner" do
-      visit "/pets/#{@pet.id}/edit"
-      expect(page).to have_field('pet_name')
-      expect(page.has_checked_field?(@owner.id)).to eq(true)
-      expect(page).to have_field('owner[name]')
     end
 
 
